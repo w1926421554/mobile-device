753d88cedc0df4e8d541f6928c4a7c3d3c07d508
吴澳冬 <1926421554@qq.com>
2024-07-19 17:53:53 +0800

    测试

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-//phoneModel.js
-// import MobileDetect from 'https://cdn.jsdelivr.net/npm/mobile-detect@1.4.5/+esm'
-// import MobileDetect from "./node_modules/mobile-detect/mobile-detect-modernizr.js";
-// const MobileDetect2 = require('mobile-detect')
-// const getIphoneModel = require('./iPhone-detect.min.js')
-// console.log("🚀 ~ file: index.js:3 ~ MobileDetect:", MobileDetect);
-//@ts-ignore
-// import {a} from './test.js'
-// console.log("🚀 ~ file: index.js:9 ~ moduleName:", a)
-
 import getIphoneModel from "./iPhone-detect.js";
 
-// phoneModel()
-console.log(getIphoneModel)
-console.log('getIphoneModel', getIphoneModel())
-// export default phoneModel;
+const str = getIphoneModel()
+let phone = document.querySelector('.phone')
+phone.innerHTML = str
+
